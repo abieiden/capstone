768109fc7bdb176a17a74f37a26a92f8389aea43
abieiden <abbi.lop33@gmail.com>
2022-02-26 01:48:37 -0800

    removed layout

--- src/App.js
+++ src/App.js
@@ -1,74 +1,63 @@
 import React from 'react';
 import './App.css';
-import { Route, Link, Routes, Outlet} from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Home, About, Collection, Contributors } from "./components";
 
 function App() {
   return (
     <div className="App">
+      <div className="navigation">
+        <nav class="navbar navbar-expand navbar-light" >
+          <div class="container-fluid" style={{maxWidth: 1500}}>
+            <Link class="navbar-brand" to="/">
+              <span class="h3" style={{}}>
+                Exploring Immigrant Identity Through Art
+              </span>
+            </Link>
+            <div>
+              <ul class="navbar-nav ml-auto">
+                <li>
+                  <Link class="nav-link" to="/">
+                    <span class="h4">
+                      Home
+                    </span>
+                  </Link>
+                </li>
+                <li>
+                  <Link class="nav-link" to="about">
+                    <span class="h4">
+                      About
+                    </span>
+                  </Link>
+                </li>
+                <li>
+                  <Link class="nav-link" to="collection">
+                    <span class="h4">
+                      Collection
+                    </span>
+                  </Link>
+                </li>
+                <li>
+                  <Link class="nav-link" to="contributors">
+                    <span class="h4">
+                      Contributors
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
       <div class="main">
         <Routes>
-          <Route exact path="/" element={<Layout />} />
-            <Route path="collection" element={<Collection />} />
-            <Route path="about" element={<About />} />
-            <Route path="contributors" element={<Contributors />} />
+          <Route path="/" element={<Home />} />
+          <Route path="collection" element={<Collection />} />
+          <Route path="about" element={<About />} />
+          <Route path="contributors" element={<Contributors />} />
         </Routes>
       </div>
     </div>
-  );
-}
-
-function Layout() {
-  return (
-    <div className = "page-layout">
-    <div className="navigation">
-    <nav class="navbar navbar-expand navbar-light" >
-      <div class="container-fluid" style={{maxWidth: 1500}}>
-        <Link class="navbar-brand" to="/">
-          <span class="h3" style={{}}>
-            Exploring Immigrant Identity Through Art
-          </span>
-        </Link>
-        <div>
-          <ul class="navbar-nav ml-auto">
-            <li>
-              <Link class="nav-link" to="/">
-                <span class="h4">
-                  Home
-                </span>
-                <span class="sr-only">(current)</span>
-              </Link>
-            </li>
-            <li>
-              <Link class="nav-link" to="/about">
-                <span class="h4">
-                  About
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link class="nav-link" to="/collection">
-                <span class="h4">
-                  Collection
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link class="nav-link" to="/contributors">
-                <span class="h4">
-                  Contributors
-                </span>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </div>
-  <div className="content">
-        <Outlet />
-  </div>
-  </div>
   );
 }
 
