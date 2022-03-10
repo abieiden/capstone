5bf0ed64118c42407c2dafb31c1dd158d59d1975
abieiden <abbi.lop33@gmail.com>
2022-03-10 03:16:34 -0800

    switch order on homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -10,36 +10,11 @@ function Home() {
     <div class = "home">
        <div class="container-fluid">
         <div class="row align-items-center">
-          <div class="col-lg-6">
-            <img
-              class="img-fluid rounded"
-              src={abilopez}
-              alt="Abi Lopez Portrait"
-              style={{width: 500, height: 'auto'}}
-            />
-          </div>
-          <div class="col-lg-6">
-            <h1 class="font-weight-light">About Abi</h1>
-            <p>
-            Abi Lopez (she/her) is a daughter of Mexican immigrants from Southeast LA. 
-            She's currently completing her undergraduate degree at Stanford University in 
-            Electrical Engineering with a minor in Human Rights. She strives to leverage her 
-            education to advance immigrant rights, racial justice, and environmental justice. 
-            Her current project is creating an immigration art exposition at Stanford for students 
-            to share their journey exploring their immigrant identity through art.
-            </p>
-            <Button variant="outline-secondary" onClick={() => navigate("about")}>Read Bio</Button>
-          </div>
-        </div>
-      </div>
-
-      <div class="container-fluid" style={{marginTop: 15}}>
-        <div class="row align-items-center">
-          <div class="col-lg-7">
+        <div class="col-lg-7">
             <img
               class="img-fluid rounded"
               src={kayumanggi}
-              alt="Art Work"
+              alt="Clip from Kayumanggi Video, submitted by Trisha Litong"
               style={{width: 500, height: 'auto'}}
             />
           </div>
@@ -55,6 +30,32 @@ function Home() {
 
             </p>
             <Button variant="outline-secondary" onClick={() => navigate("collection")}>See Collection</Button>
+          </div>
+          
+        </div>
+      </div>
+
+      <div class="container-fluid" style={{marginTop: 15}}>
+        <div class="row align-items-center">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded"
+              src={abilopez}
+              alt="Abi Lopez Portrait"
+              style={{width: 500, height: 'auto'}}
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">About Organizer</h1>
+            <p>
+            Abi Lopez (she/her) is a daughter of Mexican immigrants from Southeast LA. 
+            She's currently completing her undergraduate degree at Stanford University in 
+            Electrical Engineering with a minor in Human Rights. She strives to leverage her 
+            education to advance immigrant rights, racial justice, and environmental justice. 
+            Her current project is creating an immigration art exposition at Stanford for students 
+            to share their journey exploring their immigrant identity through art.
+            </p>
+            <Button variant="outline-secondary" onClick={() => navigate("about")}>Read Bio</Button>
           </div>
           
         </div>
